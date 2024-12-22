9ee98cd6e310c1a1ad6e0656599310e4abe040e6
sadeheden <sadeheden@gmail.com>
2024-12-22 19:16:44 +0200

    /

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Route for the homepage
 app.get("/", (req, res) => {
   res.send("Server is running successfully!");
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
