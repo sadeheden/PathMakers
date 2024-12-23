784ad79fd13dfa6b74c95558c22a2e30df1a0a57
sadeheden <sadeheden@gmail.com>
2024-12-23 10:43:20 +0200

    0

--- src/AppPage.jsx
+++ src/AppPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroSection from "../components/HeroSection";
+import HeroSection from "../components/Hero";
 import AppSection from "../components/AppSection";
 
 const HeroPage = () => {
