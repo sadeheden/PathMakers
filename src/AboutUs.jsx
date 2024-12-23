09e036a5936c0a8fc60f76be3de8ea1994f67815
mayhalaf <155309787+mayhalaf@users.noreply.github.com>
2024-12-23 10:33:57 +0200

    nn

---
+++ src/AboutUs.jsx
@@ -0,0 +1,32 @@
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./App"; // Replace with your actual homepage component
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        {/* Navigation Menu */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Define Routes */}
+        <Routes>
+          <Route path="/" element={<HomePage />} /> {/* Home Page */}
+          <Route path="/about" element={<AboutSection />} /> {/* About Us Page */}
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
